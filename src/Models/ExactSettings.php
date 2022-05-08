955b632684e0bcf1ca55ce185d5ed3dd258bd15a
pepijn <pepijn@pdik.nl>
2022-05-08 19:15:44 +0200

    Settings Model

--- src/Models/ExactSettings.php
+++ src/Models/ExactSettings.php
@@ -11,37 +11,24 @@ class ExactSettings extends Model
     protected $fillable = ['option_name', 'option_value', 'option_id'];
     protected $primaryKey = 'option_id';
 
-    /**
-     * @param $name
-     * @param $value
-     */
-    public static function setValue($name, $value)
+   public static function setValue($name, $value)
     {
-        if (config('exact.type') == 'multiuser') {
-            //For future
-        } elseif ($name != "client_id" || $name != "client_secret" || $name != "webhook_secret") {
-            $s = ExactSettings::where('option_name', $name)->firstOrnew();
-            $s->option_name = $name;
-            $s->option_value = $value;
-            $s->save();
-        }
+        $s= settings::where('option_name', $name)->firstOrnew();
+        $s->option_name = $name;
+        $s->option_value = $value;
+        $s->save();
     }
 
     /**
      * @param $name
      * @return mixed
      */
-    public static function getValue($key, $default = null)
-    {
-        if ($key != "client_id" || $key != "client_secret" || $key != "webhook_secret") {
-            return config('exact.'.$key.'');
-        } else {
-            $setting = ExactSettings::where('option_name', '=', $key)->first();
-            if ($setting) {
-                return $setting->option_value;
-            }
-            //When not in DB get default config value for one connection
-            return $default;
+     public static function getValue($key, $default = null){
+        $setting = settings::where('option_name', '=', $key)->first();
+        if($setting){
+            return $setting->option_value;
         }
+        return $default;
     }
+
 }
